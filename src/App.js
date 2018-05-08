be88e3a0038d7dfca9b4d53083be4969f20a991a
Tetsutaro Endo <tetsutaro.endo@gmail.com>
2018-05-08 19:18:41 +0900

    OGPの設定

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet'
 
 class App extends Component {
   render() {
@@ -13,6 +14,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Helmet
+         title="[title] Netlify Prerendering Test"
+         meta={[
+           {name: "description", content: "[description] Netlify Prerendering Test"},
+           {name: "og:title", content: "[og:title] Netlify Prerendering Test" },
+           {name: "og:type", content: "website" },
+           {name: "og:url", content: "https://netlify-prerendering-test.com" },
+           {name: "og:image", content: "/ogp_image.png" },
+           {name: "og:description", content: "[og:description] Netlify Prerendering Test"},
+         ]}
+        />
       </div>
     );
   }
